28f175ee58e8ab17dc67da56ff571fa7e1e40d92
Philipp Mayr <philipp.mayr2@tha.de>
2025-10-23 09:20:51 +0200

    Initial Commit

---
+++ virtualdrums/virtualdrumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtualdrumsApp.swift
+//  virtualdrums
+//
+//  Created by Passion on 23.10.25.
+//
+
+import SwiftUI
+
+@main
+struct virtualdrumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
