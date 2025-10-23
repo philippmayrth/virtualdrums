c6716de7963e04c873500e23867a00b002913773
Philipp Mayr <philipp.mayr2@tha.de>
2025-10-23 22:07:39 +0200

    Add MIDIManager for MIDI output and simulator logging in DrumVolumeView

--- virtualdrums/DrumVolumeView.swift
+++ virtualdrums/DrumVolumeView.swift
@@ -2,10 +2,50 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 import AVFoundation
+import CoreMIDI
+
+class MIDIManager {
+    private var client = MIDIClientRef()
+    private var outPort = MIDIPortRef()
+    private var destination: MIDIEndpointRef? = nil
+    
+    init() {
+        MIDIClientCreate("VirtualDrumsMIDIClient" as CFString, nil, nil, &client)
+        MIDIOutputPortCreate(client, "VirtualDrumsOutPort" as CFString, &outPort)
+        // Use the first available destination
+        let destCount = MIDIGetNumberOfDestinations()
+        if destCount > 0 {
+            destination = MIDIGetDestination(0)
+        } else {
+            print("No MIDI destinations available.")
+        }
+    }
+    
+    func sendNoteOn(note: UInt8 = 60, velocity: UInt8 = 100, channel: UInt8 = 9) {
+        #if targetEnvironment(simulator)
+        print("[SIMULATOR] MIDI Note On - note: \(note), velocity: \(velocity), channel: \(channel)")
+        #else
+        guard let destination = destination else { return }
+        var packet = MIDIPacket()
+        packet.timeStamp = 0
+        packet.length = 3
+        packet.data.0 = 0x90 | (channel & 0x0F) // Note On, channel
+        packet.data.1 = note // MIDI note number
+        packet.data.2 = velocity // Velocity
+        var packetList = MIDIPacketList(numPackets: 1, packet: packet)
+        withUnsafePointer(to: &packetList) { ptr in
+            let rawPtr = UnsafeRawPointer(ptr)
+            let midiPtr = rawPtr.bindMemory(to: MIDIPacketList.self, capacity: 1)
+            MIDISend(outPort, destination, midiPtr)
+        }
+        #endif
+    }
+}
 
 struct DrumVolumeView: View {
     @State private var message: String = "Touch the drum!"
     @State private var audioPlayer: AVAudioPlayer? = nil
+    private let midiManager = MIDIManager()
     
     var body: some View {
         ZStack {
@@ -26,6 +66,7 @@ struct DrumVolumeView: View {
                     .onEnded {
                         message = "Drum tapped!"
                         playDrumSound()
+                        midiManager.sendNoteOn(note: 36, velocity: 120, channel: 0) // MIDI note 36 = Bass Drum
                         print("Drum entity tapped!")
                     }
             )
