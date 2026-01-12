a9c06bb4ae03e55778e160101cbeddddaba95df7
Oliver Hans Kühle <oliver.hans.kuehle@tha.de>
2026-01-12 20:04:27 +0100

    refactor: Move CollisionGroup extension to a new file

---
+++ virtualdrums/CollisionGroup.swift
@@ -0,0 +1,8 @@
+
+import RealityKit
+
+extension CollisionGroup {
+    static let drum = CollisionGroup(rawValue: 1 << 0)
+    static let stickTipLeft = CollisionGroup(rawValue: 1 << 1)
+    static let stickTipRight = CollisionGroup(rawValue: 1 << 2)
+}
--- virtualdrums/ImmersiveViewModel.swift
+++ virtualdrums/ImmersiveViewModel.swift
@@ -10,11 +10,7 @@ import Combine
 import RealityKit
 import SwiftUI
 
-extension CollisionGroup {
-    static let drum = CollisionGroup(rawValue: 1 << 0)
-    static let stickTipLeft = CollisionGroup(rawValue: 1 << 1)
-    static let stickTipRight = CollisionGroup(rawValue: 1 << 2)
-}
+
 
 struct StickState {
     let stickEntity: Entity
